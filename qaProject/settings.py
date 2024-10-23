86283d687073bec9f4cc71b74e0e66039248bd8b
Ben Young <ben.young999@hotmail.co.uk>
2024-10-23 11:45:20 +0100

    Initial commit

--- qaProject/settings.py
+++ qaProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-p3v4x6@v+rt*#khp&5pj1(^+kvv2p4t4hiyuqcjid@dkywu94u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['qaproject.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
+# Use the PORT environment variable if it exists
+PORT = os.getenv('PORT', '8000')
 
 # Application definition
 
